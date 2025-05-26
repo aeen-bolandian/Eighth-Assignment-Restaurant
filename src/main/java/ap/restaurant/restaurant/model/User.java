83748d5a3fd4eeb75c9a24afa9ad8b0214349e38
aeen bolandian <bolandian85@gmail.com>
2025-05-26 14:43:47 +0330

    user attributes added

--- src/main/java/ap/restaurant/restaurant/model/User.java
+++ src/main/java/ap/restaurant/restaurant/model/User.java
@@ -1,4 +1,40 @@
 package ap.restaurant.restaurant.model;
 
+import java.util.List;
+import java.util.UUID;
+
 public class User {
+    private UUID id;
+    private String username;
+    private String password;
+    private String email;
+    private List<String> feedbacks;
+    private List<Order> orders;
+
+    // getters :
+    // --------------------------------
+    public String getEmail() {
+        return email;
+    }
+
+    public List<String> getFeedbacks() {
+        return feedbacks;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    // --------------------------------
 }
