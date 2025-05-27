45beda916634f23894fb4ff2c9c208809e850a97
aeen bolandian <bolandian85@gmail.com>
2025-05-27 09:58:28 +0330

    OrderDetails attributes added

--- src/main/java/ap/restaurant/restaurant/model/OrderDetails.java
+++ src/main/java/ap/restaurant/restaurant/model/OrderDetails.java
@@ -1,4 +1,35 @@
 package ap.restaurant.restaurant.model;
 
+import java.util.UUID;
+
 public class OrderDetails {
+    private UUID id;
+    private UUID orderId;
+    private UUID menuItemId;
+    private int quantity;
+    private double price;
+
+    // Getters :
+    // --------------------------------
+
+    public UUID getId() {
+        return id;
+    }
+
+    public UUID getMenuItemId() {
+        return menuItemId;
+    }
+
+    public UUID getOrderId() {
+        return orderId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    // --------------------------------
 }
