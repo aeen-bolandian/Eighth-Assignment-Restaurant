3a667328d359ecb620ecce61778d083458797564
aeen bolandian <bolandian85@gmail.com>
2025-05-28 00:28:24 +0330

    add basic front for menu and menuItem

---
+++ src/main/java/ap/restaurant/restaurant/controller/MenuController.java
@@ -0,0 +1,28 @@
+package ap.restaurant.restaurant.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuController implements Initializable {
+
+    @FXML
+    private ScrollPane menuScrollPane;
+
+    @FXML
+    private Button orderButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
+
+    @FXML
+    public void order(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/ap/restaurant/restaurant/controller/MenuItemController.java
@@ -0,0 +1,20 @@
+package ap.restaurant.restaurant.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class MenuItemController {
+
+    @FXML
+    private Label menuItemName;
+
+    @FXML
+    private TextArea description;
+
+    @FXML
+    private Button menuItemOrderButton;
+
+
+}
