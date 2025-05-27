af0311e2e396acef302b7625144b85004f526dff
aeen bolandian <bolandian85@gmail.com>
2025-05-27 09:49:45 +0330

    Order attributes added

--- src/main/java/ap/restaurant/restaurant/model/Order.java
+++ src/main/java/ap/restaurant/restaurant/model/Order.java
@@ -1,4 +1,30 @@
 package ap.restaurant.restaurant.model;
 
+import java.util.Date;
+import java.util.UUID;
+
 public class Order {
+    private UUID id;
+    private UUID userId;
+    private Date createdAt;
+    private double totalPrice;
+
+    // Getters :
+    // --------------------------------
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
 }
