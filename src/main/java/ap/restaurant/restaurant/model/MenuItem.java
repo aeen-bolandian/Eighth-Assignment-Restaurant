2ddee8d0fa6740f7a878c86fa036800367e0a2aa
aeen bolandian <bolandian85@gmail.com>
2025-05-27 09:48:02 +0330

    MenuItem attributes added

--- src/main/java/ap/restaurant/restaurant/model/MenuItem.java
+++ src/main/java/ap/restaurant/restaurant/model/MenuItem.java
@@ -1,4 +1,40 @@
 package ap.restaurant.restaurant.model;
 
+import java.util.UUID;
+
 public class MenuItem {
+    private UUID id;
+    private String name;
+    private double price;
+    private String description;
+    private double rating;
+    private int choiceNums;
+
+    // Getters :
+    // --------------------------------
+
+    public int getChoiceNums() {
+        return choiceNums;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+    // --------------------------------
 }
