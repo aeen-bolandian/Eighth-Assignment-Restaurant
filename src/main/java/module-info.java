523552de7f9223a9a53f762c18569c5dc732a27f
aeen bolandian <bolandian85@gmail.com>
2025-06-04 03:40:16 +0330

    add css stylesheets

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module ap.restaurant.restaurant {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires jbcrypt;
 
 
     opens ap.restaurant.restaurant to javafx.fxml;
